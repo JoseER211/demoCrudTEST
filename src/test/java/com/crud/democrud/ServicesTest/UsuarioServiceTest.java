f454083f6dbaabfa778feae216eb69a0102abc26
JoseER211 <88160386+JoseER211@users.noreply.github.com>
2022-04-27 21:56:10 -0500

    Se agregaron pruebas unitarias

--- src/test/java/com/crud/democrud/ServicesTest/UsuarioServiceTest.java
+++ src/test/java/com/crud/democrud/ServicesTest/UsuarioServiceTest.java
@@ -1,6 +1,8 @@
 package com.crud.democrud.ServicesTest;
 
 import com.crud.democrud.models.UsuarioModel;
+import com.crud.democrud.models.UsuarioRol;
+import com.crud.democrud.repositories.RolRepository;
 import com.crud.democrud.repositories.UsuarioRepository;
 import com.crud.democrud.services.UsuarioService;
 import org.junit.jupiter.api.Test;
@@ -11,34 +13,51 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.List;
 import java.util.Optional;
 
-import static  org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioServiceTest {
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @Autowired
+    RolRepository rolRepository;
+
     @Test
-    public void testGuardarUsuario(){
-        UsuarioModel usuarioModel=new UsuarioModel("aquaman","aqua@gmail.com",99);
+    public void testGuardarUsuario() {
+        UsuarioModel usuarioModel = new UsuarioModel("aquaman", "aqua@gmail.com", 99);
         UsuarioModel usuarioModelRegistrado = usuarioRepository.save(usuarioModel);
         assertNotNull(usuarioModelRegistrado);
     }
 
     @Test
-    public void testBuscarUsuarioPorId(){
-        Long idBuscado=1L;
-        Optional<UsuarioModel> usuarioModelBuscado=usuarioRepository.findById(idBuscado);
+    public void testBuscarUsuarioPorId() {
+        Long idBuscado = 1L;
+        Optional<UsuarioModel> usuarioModelBuscado = usuarioRepository.findById(idBuscado);
         assertThat(usuarioModelBuscado.get().getId()).isEqualTo(idBuscado);
     }
 
     @Test
-    public void testListarUsuarios(){
-        List<UsuarioModel> usuarioModelList=(List<UsuarioModel>) usuarioRepository.findAll();
+    public void testListarUsuarios() {
+        List<UsuarioModel> usuarioModelList = (List<UsuarioModel>) usuarioRepository.findAll();
         assertThat(usuarioModelList).size().isGreaterThan(0);
     }
 
-    
+
+    @Test
+    public void testBuscarRolPorId() {
+        Long idBuscado = 1L;
+        Optional<UsuarioRol> rolBuscado = rolRepository.findById(idBuscado);
+        assertThat(rolBuscado.get().getId()).isEqualTo(idBuscado);
+    }
+
+    @Test
+    public void testListarRoles() {
+        List<UsuarioRol> rolList = (List<UsuarioRol>) rolRepository.findAll();
+        assertThat(rolList).size().isGreaterThan(0);
+    }
+
+
 }
